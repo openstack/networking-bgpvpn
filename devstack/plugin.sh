ac6ab470358fbdb45afcd4f2bd2ba132447bd741
Thomas Morin <thomas.morin@orange.com>
2015-10-09 16:15:30 +0200

    Fix devstack plugin.sh
    Do the copy of config file at the 'config' stage.
    (/etc/neutron does not exist yet at the 'install' stage)
    
    Change-Id: I9d98356fa864aada125d87893bfb5c3aeba347eb

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -8,13 +8,16 @@ if [[ "$1" == "source" ]]; then
 	# no-op
 	:
 elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+	echo_summary "Installing networking-bgpvpn"
 	setup_develop $NETWORKING_BGPVPN_DIR
- 	mkdir -p $NEUTRON_CONF_DIR/policy.d && cp $NETWORKING_BGPVPN_DIR/etc/neutron/policy.d/bgpvpn.conf $NEUTRON_CONF_DIR/policy.d
-	mkdir -p $(dirname $NETWORKING_BGPVPN_CONF) && cp $NETWORKING_BGPVPN_DIR/etc/neutron/networking_bgpvpn.conf $NETWORKING_BGPVPN_CONF
 elif [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
+	echo_summary "Enabling networking-bgpvpn service plugin"
 	_neutron_service_plugin_class_add $BGPVPN_PLUGIN_CLASS
 elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         if is_service_enabled q-svc; then
+                echo_summary "Configuring networking-bgpvpn"
+                mkdir -v -p $NEUTRON_CONF_DIR/policy.d && cp -v $NETWORKING_BGPVPN_DIR/etc/neutron/policy.d/bgpvpn.conf $NEUTRON_CONF_DIR/policy.d
+                mkdir -v -p $(dirname $NETWORKING_BGPVPN_CONF) && cp -v $NETWORKING_BGPVPN_DIR/etc/neutron/networking_bgpvpn.conf $NETWORKING_BGPVPN_CONF
                 inicomment $NETWORKING_BGPVPN_CONF service_providers service_provider
                 iniadd $NETWORKING_BGPVPN_CONF service_providers service_provider $NETWORKING_BGPVPN_DRIVER
         fi
