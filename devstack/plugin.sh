2804d5f01afbbb10d78a4c4ff519895f69178f6c
Mathieu Rohon <mathieu.rohon@gmail.com>
2016-02-18 10:38:48 +0000

    Devstack : configure tempest file during extra hook
    The bgpvpn devstack plugin currently configures tempest during
    the post-config stage. But the tempest.conf file is overwritten
    by tempest during the extra stage of devstack, hence erasing
    modifications made by the bgpvpn plugin.
    
    To fix this issue, the modification of the tempest.conf file
    must be done during the extra stage of devstack.
    
    This patch also corrects the indentation of the devstack plugin.
    
    Change-Id: Ia2e76f1c9aea93e481694b0821d1e5c3bed4da11
    Closes-bug: #1548364

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -5,33 +5,35 @@ XTRACE=$(set +o | grep xtrace)
 set +o xtrace
 
 if [[ "$1" == "source" ]]; then
-	# no-op
-	:
+    # no-op
+    :
 elif [[ "$1" == "stack" && "$2" == "install" ]]; then
-	echo_summary "Installing networking-bgpvpn"
-	setup_develop $NETWORKING_BGPVPN_DIR
+    echo_summary "Installing networking-bgpvpn"
+    setup_develop $NETWORKING_BGPVPN_DIR
 elif [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
-	echo_summary "Enabling networking-bgpvpn service plugin"
-	_neutron_service_plugin_class_add $BGPVPN_PLUGIN_CLASS
+    echo_summary "Enabling networking-bgpvpn service plugin"
+    _neutron_service_plugin_class_add $BGPVPN_PLUGIN_CLASS
+elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+    if is_service_enabled tempest; then
+        echo_summary "Enabling bgpvpn in $TEMPEST_CONFIG"
+        iniset $TEMPEST_CONFIG service_available bgpvpn "True"
+    fi
 elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
-        if is_service_enabled q-svc; then
-                echo_summary "Configuring networking-bgpvpn"
-                mkdir -v -p $NEUTRON_CONF_DIR/policy.d && cp -v $NETWORKING_BGPVPN_DIR/etc/neutron/policy.d/bgpvpn.conf $NEUTRON_CONF_DIR/policy.d
-                mkdir -v -p $(dirname $NETWORKING_BGPVPN_CONF) && cp -v $NETWORKING_BGPVPN_DIR/etc/neutron/networking_bgpvpn.conf $NETWORKING_BGPVPN_CONF
-                inicomment $NETWORKING_BGPVPN_CONF service_providers service_provider
-                iniadd $NETWORKING_BGPVPN_CONF service_providers service_provider $NETWORKING_BGPVPN_DRIVER
-        fi
-        if is_service_enabled tempest; then
-                iniadd $TEMPEST_CONFIG service_available bgpvpn True
-        fi
+    if is_service_enabled q-svc; then
+        echo_summary "Configuring networking-bgpvpn"
+        mkdir -v -p $NEUTRON_CONF_DIR/policy.d && cp -v $NETWORKING_BGPVPN_DIR/etc/neutron/policy.d/bgpvpn.conf $NEUTRON_CONF_DIR/policy.d
+        mkdir -v -p $(dirname $NETWORKING_BGPVPN_CONF) && cp -v $NETWORKING_BGPVPN_DIR/etc/neutron/networking_bgpvpn.conf $NETWORKING_BGPVPN_CONF
+        inicomment $NETWORKING_BGPVPN_CONF service_providers service_provider
+        iniadd $NETWORKING_BGPVPN_CONF service_providers service_provider $NETWORKING_BGPVPN_DRIVER
+    fi
 fi
 if [[ "$1" == "unstack" ]]; then
-	#no-op
-	:
+    #no-op
+    :
 fi
 if [[ "$1" == "clean" ]]; then
-	#no-op
-	:
+    #no-op
+    :
 fi
 
 set +x
