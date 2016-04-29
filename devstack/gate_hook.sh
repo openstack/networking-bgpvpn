c31b679bd637e40c9e500208d75b03aecdec9e7c
Thomas Morin <thomas.morin@orange.com>
2016-04-29 16:30:52 -0500

    devstack: fix OVS compilation hook
    Follow changes made in how Neutron devstack handles
    compiling a fresh OVS (see Ia7ad1658b95d7404384c7cae833008a57e3e5af1).
    
    The benefit is that our devstack code is simplified.
    
    Change-Id: I3abc2ac4ce2d4710f998e7749d1f5df4303533e2

--- devstack/gate_hook.sh
+++ devstack/gate_hook.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
 
 GATE_DEST=$BASE/new
-DEVSTACK_PATH=$GATE_DEST/devstack
-NEUTRON_PATH=$GATE_DEST/neutron
-
-if [[ "$COMPILE_OVS" == "True" ]]; then
-	source $DEVSTACK_PATH/functions
-	source $NEUTRON_PATH/devstack/lib/ovs
-
-	# The OVS_BRANCH variable is used by git checkout.
-	OVS_BRANCH=branch-2.4
-	echo "Compiling OVS $OVS_BRANCH..."
-	for package in openvswitch openvswitch-switch openvswitch-common; do
-	    if is_package_installed $package; then
-		uninstall_package $package
-	    fi
-	done
-	compile_ovs True /usr
-	echo "Starting new OVS..."
-	start_new_ovs
-fi
 
 sudo modprobe bridge
 
