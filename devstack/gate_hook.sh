59799d0ab2ab3081bea43119f1988dc7aa6dadbd
Pierre Crégut <pierre.cregut@orange.com>
2016-10-18 11:00:13 +0200

    migration tests as functional tests.
    - migration tests now have time to execute
    - add hooks to have database configured for functional tests.
    - add hook to see result of tests.
    
    Change-Id: I0d2406ef10df897fabe44e6fc287ea292843ecce
    Closes-Bug: 1632212

--- devstack/gate_hook.sh
+++ devstack/gate_hook.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
+set -xe
+
+testtype=${1:-"unknown"}
 GATE_DEST=$BASE/new
 
 sudo modprobe bridge
 
-$GATE_DEST/devstack-gate/devstack-vm-gate.sh
+case "$testtype" in
+dsvm-bagpipe-functional*|dsvm-functional*)
+    # Used by configure_for_func_testing
+    DEVSTACK_PATH=$GATE_DEST/devstack
+    IS_GATE=True
+    source "$GATE_DEST"/neutron/tools/configure_for_func_testing.sh
+    configure_host_for_func_testing
+    ;;
+*)
+    $GATE_DEST/devstack-gate/devstack-vm-gate.sh
+esac
 
--- devstack/post_test_hook.sh
+++ devstack/post_test_hook.sh
@@ -1,2 +1,41 @@
-# place holder until we have something useful to do here
+#!/usr/bin/env bash
+
+set -xe
+
+NETWORKING_BGPVPN_DIR="$BASE/new/networking-bgpvpn"
+LOGS=/opt/stack/logs
+GATE_STACK_USER=stack
+
+typetest=${1:-"unknown"}
+
+function generate_testr_results {
+    sudo -H -u $GATE_STACK_USER chmod o+rw .
+    sudo -H -u $GATE_STACK_USER chmod o+rw -R .testrepository
+    if [ -f ".testrepository/0" ]; then
+        .tox/$venv/bin/subunit-1to2 < .testrepository/0  > ./testrepository.subunit
+        .tox/$venv/bin/subunit2html ./testrepository.subunit ./testr_results.html
+        gzip ./testrepository.subunit   
+        gzip ./testr_results.html
+        sudo mv ./testrepository.subunit.gz testr_results.html.gz $LOGS
+    fi
+}
+
+case "$typetest" in
+dsvm-bagpipe-functional*|dsvm-functional*)
+    cd $NETWORKING_BGPVPN_DIR
+    sudo chown -R $GATE_STACK_USER:$GATE_STACK_USER .
+    # Run tests
+    venv=${typetest/dsvm-bagpipe/dsvm}
+    echo "Running networking-bgpvpn $venv test suite"
+    set +e
+    sudo -H -u $GATE_STACK_USER tox -e ${venv}
+    testr_exit_code=$?
+    set -e
+    # Collect and parse results
+    generate_testr_results
+    exit $testr_exit_code
+    ;;
+*)
+    ;;
+esac
 
